568acdb7f97a3c7b3bbfe0c1ad5d97895f41aa51
Soroush Kazemi <kazemi.soroush@gmail.com>
2017-11-10 13:27:12 +0330

    adds encryption for credential username and password.

--- src/Ntm/Model/SshCredential.php
+++ src/Ntm/Model/SshCredential.php
@@ -3,7 +3,6 @@
 namespace Ntcm\Ntm\Model;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
  * @author Soroush Kazemi <kazemi.soroush@gmail.com>
@@ -36,5 +35,45 @@ class SshCredential extends Model {
     public function getTable()
     {
         return table_name('ssh_credentials');
+    }
+
+    /**
+     * Encrypt the username when you need to store it.
+     *
+     * @param $username
+     */
+    public function setUsernameAttribute($username)
+    {
+        $this->attributes['username'] = encrypt($username);
+    }
+
+    /**
+     * Encrypt the password when you need to store it.
+     *
+     * @param $password
+     */
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = encrypt($password);
+    }
+
+    /**
+     * Encrypt the username when you need to store it.
+     *
+     * @return string
+     */
+    public function getUsernameAttribute()
+    {
+        return decrypt($this->attributes['username']);
+    }
+
+    /**
+     * Encrypt the password when you need to store it.
+     *
+     * @return string
+     */
+    public function getPasswordAttribute()
+    {
+        return decrypt($this->attributes['password']);
     }
 }
