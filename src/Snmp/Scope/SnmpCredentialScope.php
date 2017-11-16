e972c37e61bb6fa328bae9f0aa944917fb651a60
Soroush Kazemi <kazemi.soroush@gmail.com>
2017-11-16 14:59:31 +0330

    fixes snmp credential scope.

--- src/Snmp/Scope/SnmpCredentialScope.php
+++ src/Snmp/Scope/SnmpCredentialScope.php
@@ -17,20 +17,15 @@ trait SnmpCredentialScope {
      */
     public function scopeFindOrCreate($query, $attributes)
     {
-        $credential = $query->where('address', $attributes['address'])
-                            ->get()
-                            ->filter(function ($record) use ($attributes) {
-                                if($record->username == $attributes['username']) {
-                                    return $record;
-                                }
-                            })
-                            ->first();
+        $credential = $query->where('address', $attributes['address'])->first();
 
-        if( ! $credential) {
-            $credential = $query->create($attributes);
-        } else {
-            $credential->update($attributes);
+        // remove the previous snmp credentials...
+        if($credential) {
+            $credential->destroy();
         }
+
+        // persist the new one...
+        $credential = $query->create($attributes);
 
         // if the credential found return it...
         return $credential;
