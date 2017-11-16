f879ab559333559743937bc2edea0d436f9e2d00
Soroush Kazemi <kazemi.soroush@gmail.com>
2017-11-16 13:24:19 +0330

    adds some doc block.

--- src/Ntm/NtmFacade.php
+++ src/Ntm/NtmFacade.php
@@ -4,8 +4,14 @@ namespace Ntcm\Ntm;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @author Soroush Kazemi <kazemi.soroush@gmail.com>
+ */
 class NtmFacade extends Facade {
 
+    /**
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'ntm';
--- src/Snmp/SnmpFacade.php
+++ src/Snmp/SnmpFacade.php
@@ -4,8 +4,14 @@ namespace Ntcm\Snmp;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @author Soroush Kazemi <kazemi.soroush@gmail.com>
+ */
 class SnmpFacade extends Facade {
 
+    /**
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'snmp';
