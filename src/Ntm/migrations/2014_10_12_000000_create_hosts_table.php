358482275fe3e29d478ac53da3b6d38e11bdd012
Soroush Kazemi <kazemi.soroush@gmail.com>
2018-04-01 11:58:36 +0430

    removes host-scan relationship.

--- src/Ntm/migrations/2014_10_12_000000_create_hosts_table.php
+++ src/Ntm/migrations/2014_10_12_000000_create_hosts_table.php
@@ -48,9 +48,9 @@ class CreateHostsTable extends Migration {
      */
     public function down()
     {
-        Schema::table($this->getTable(), function (Blueprint $table) {
-            $table->dropForeign(['scan_id']);
-        });
+        //Schema::table($this->getTable(), function (Blueprint $table) {
+        //    $table->dropForeign(['scan_id']);
+        //});
 
         Schema::drop($this->getTable());
     }
