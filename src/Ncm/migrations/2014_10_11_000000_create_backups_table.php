95ae2ecb07e0fefae76f42e0437a8d2f458eafe3
Soroush Kazemi <kazemi.soroush@gmail.com>
2018-04-05 16:37:34 +0430

    fixes some migration names.

--- src/Ncm/migrations/2014_10_11_000000_create_backups_table.php
+++ src/Ncm/migrations/2014_10_11_000000_create_backups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBackupsGenericTable extends Migration {
+class CreateBackupsTable extends Migration {
 
     /**
      * Get table name.
