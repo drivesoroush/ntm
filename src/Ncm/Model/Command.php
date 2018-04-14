eae9241fdd68f0f1b74f7cbfabd5a8ebba25d882
Soroush Kazemi <kazemi.soroush@gmail.com>
2018-04-14 14:52:36 +0430

    adds execute method for command.

--- src/Ncm/Model/Command.php
+++ src/Ncm/Model/Command.php
@@ -3,10 +3,13 @@
 namespace Ntcm\Ncm\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Ntcm\Exceptions\ProcessExecutionFailedException;
 use Ntcm\Ncm\Relation\CommandRelation;
 use Ntcm\Ncm\Scope\CommandScope;
+use Ntcm\Ntm\Model\Host;
 use Ntcm\Ntm\Restorable;
 use Carbon\Carbon;
+use Ntcm\Ntm\Util\ProcessExecutor;
 
 /**
  * @author Soroush Kazemi <kazemi.soroush@gmail.com>
@@ -42,4 +45,70 @@ class Command extends Model {
         return table_name('commands');
     }
 
+    /**
+     * Execute this command on host.
+     *
+     * @param string $ip
+     *
+     * @return string
+     * @throws ProcessExecutionFailedException
+     */
+    public function executeByIp(string $ip)
+    {
+        return $this->executeByAddress(encode_ip($ip));
+    }
+
+    /**
+     * Execute this command on host.
+     *
+     * @param int $address
+     *
+     * @return string
+     * @throws ProcessExecutionFailedException
+     */
+    public function executeByAddress(int $address)
+    {
+        $host = Host::where('address', $address)->firstOrFail();
+
+        return $this->execute($host);
+    }
+
+    /**
+     * Execute this command on host.
+     *
+     * @param Host $host
+     *
+     * @return string
+     * @throws ProcessExecutionFailedException
+     */
+    public function execute(Host $host)
+    {
+        // 192.168.101.7 22 jg @sss123 cisco_ios "show run"
+        $credential = $host->sshCredentials()->where('is_valid', true)->firstOrFail();
+        $deviceType = $host->osGeneric->alias;
+
+        $executor = new ProcessExecutor();
+
+        // execute the restore script...
+        $command = sprintf('%s %s %s %s %s %s "%s"',
+            $this->getExecutable(),
+            $host->ip,
+            $credential->port,
+            $credential->username,
+            $credential->password,
+            $deviceType,
+            $this->command
+        );
+
+        // run the restore command...
+        return $executor->execute($command, config('ncm.timeout'));
+    }
+
+    /**
+     * @return string
+     */
+    public function getExecutable()
+    {
+        return "python3.6 " . remote_config_script_path("show.py");
+    }
 }
