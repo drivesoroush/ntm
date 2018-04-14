b534624f3d4b1c25b49092616df28f13645e47b5
Soroush Kazemi <kazemi.soroush@gmail.com>
2018-04-14 16:32:39 +0430

    removes file get content utf8.

--- src/Ncm/Backupable.php
+++ src/Ncm/Backupable.php
@@ -105,7 +105,7 @@ trait Backupable {
         $filePath = tftp_path($fileName);
 
         // fetch the content...
-        $content = file_get_contents_utf8($filePath);
+        $content = file_get_contents($filePath);
 
         // remove the backup file...
         unlink_if_exists($filePath);
